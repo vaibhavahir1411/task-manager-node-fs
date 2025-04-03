fc3223cc664c799087f0c3e85741b7b9277496e8
Vaibhav <vaibhavmakvana14118@gmail.com>
2025-04-03 14:53:19 +0530

    add edit mode

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ app.use(express.static(Path.join(__dirname,"public")))
 
 app.get("/",function (req,res) {
     fs.readdir(`./tasks`,function (err,tasks){
+        //task sort by time 
+        tasks.sort((a, b) => {
+            const aTime = fs.statSync(`./tasks/${a}`).birthtimeMs;
+            const bTime = fs.statSync(`./tasks/${b}`).birthtimeMs;
+            return bTime - aTime; 
+        });
         res.render("index",{tasks : tasks});
     })  
 })
@@ -27,6 +33,26 @@ app.get("/delete/:filename",function (req,res) {
         res.redirect("/")
     }) 
 })
+
+app.get("/edit/:filename", function (req, res) {
+    fs.readFile(`./tasks/${req.params.filename}`, "utf-8", function (err, filedata) {
+        res.render("edit", { filename: req.params.filename, filedata: filedata });
+    });
+});
+
+
+app.post("/update/:filename", function (req, res) {
+    const newTitle = req.body.title;
+    const newDesc = req.body.desc;
+
+
+    fs.unlink(`./tasks/${req.params.filename}`, function (err) {
+        fs.writeFile(`./tasks/${newTitle}`, newDesc, function (err) {
+            res.redirect("/");
+        });
+    });
+});
+
 
 app.post("/create",function (req,res) {
     fs.writeFile(`./tasks/${req.body.title}`,req.body.desc,function (err) {
