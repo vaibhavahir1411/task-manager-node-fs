19cfe8d820f32b22cbf96a665d8865d75dc4b332
Vaibhav <vaibhavmakvana14118@gmail.com>
2025-04-03 13:26:58 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const Path = require("path");
+const fs = require("fs");
+
+app.set("view engine","ejs");
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+app.use(express.static(Path.join(__dirname,"public")))
+
+app.get("/",function (req,res) {
+    fs.readdir(`./tasks`,function (err,tasks){
+        res.render("index",{tasks : tasks});
+    })  
+})
+
+app.get("/file/:filename",function (req,res) {
+    fs.readFile(`./tasks/${req.params.filename}`,"utf-8",function (err,filedata) {
+        res.render("showtask",{filename : req.params.filename,filedata : filedata})
+        
+    }) 
+})
+
+app.get("/delete/:filename",function (req,res) {
+    fs.unlink(`./tasks/${req.params.filename}`,function (err,filedata) {
+        console.log('file deleted');
+        res.redirect("/")
+    }) 
+})
+
+app.post("/create",function (req,res) {
+    fs.writeFile(`./tasks/${req.body.title}`,req.body.desc,function (err) {
+        res.redirect("/")
+    })
+    
+})
+
+app.listen(3000)
