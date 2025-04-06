a99feac66f66bb2b714d7aadeca5ecb5f8a6a564
Vaibhav <vaibhavmakvana14118@gmail.com>
2025-04-06 13:30:37 +0530

    Add individual task support using cookie-based user IDs

--- index.js
+++ index.js
@@ -2,79 +2,84 @@ const express = require("express");
 const app = express();
 const Path = require("path");
 const fs = require("fs");
+const cookieParser = require("cookie-parser");
+const { v4: uuidv4 } = require("uuid");
 
-app.set("view engine","ejs");
+app.set("view engine", "ejs");
 app.use(express.json());
-app.use(express.urlencoded({extended : true}));
-app.use(express.static(Path.join(__dirname,"public")))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(Path.join(__dirname, "public")));
+app.use(cookieParser());
+
+// Assign a unique user ID using cookies
+app.use((req, res, next) => {
+    if (!req.cookies.userId) {
+        const userId = uuidv4();
+        res.cookie("userId", userId, { maxAge: 30 * 24 * 60 * 60 * 1000 }); // 30 days
+        const userFolder = `./tasks/${userId}`;
+        if (!fs.existsSync(userFolder)) fs.mkdirSync(userFolder, { recursive: true });
+    }
+    next();
+});
 
 app.get("/", function (req, res) {
-    const tasksFolder = "./tasks";
+    const userFolder = `./tasks/${req.cookies.userId}`;
 
-    // If the tasks folder doesn't exist, create it
-    if (!fs.existsSync(tasksFolder)) {
-        fs.mkdirSync(tasksFolder);
+    if (!fs.existsSync(userFolder)) {
+        fs.mkdirSync(userFolder);
     }
 
-    fs.readdir(tasksFolder, function (err, tasks) {
-        if (err) {
-            console.error("Error reading tasks folder:", err);
-            return res.render("index", { tasks: [] }); // render empty list if error
-        }
+    fs.readdir(userFolder, function (err, tasks) {
+        if (err || !Array.isArray(tasks)) return res.render("index", { tasks: [] });
 
-        if (!Array.isArray(tasks)) tasks = [];
-
-        // Sort tasks by creation time (latest first)
         tasks.sort((a, b) => {
-            const aTime = fs.statSync(`${tasksFolder}/${a}`).birthtimeMs;
-            const bTime = fs.statSync(`${tasksFolder}/${b}`).birthtimeMs;
+            const aTime = fs.statSync(`${userFolder}/${a}`).birthtimeMs;
+            const bTime = fs.statSync(`${userFolder}/${b}`).birthtimeMs;
             return bTime - aTime;
         });
 
-        res.render("index", { tasks: tasks });
+        res.render("index", { tasks });
     });
 });
 
+app.get("/file/:filename", function (req, res) {
+    const userFolder = `./tasks/${req.cookies.userId}`;
+    fs.readFile(`${userFolder}/${req.params.filename}`, "utf-8", function (err, filedata) {
+        res.render("showtask", { filename: req.params.filename, filedata });
+    });
+});
 
-app.get("/file/:filename",function (req,res) {
-    fs.readFile(`./tasks/${req.params.filename}`,"utf-8",function (err,filedata) {
-        res.render("showtask",{filename : req.params.filename,filedata : filedata})
-        
-    }) 
-})
-
-app.get("/delete/:filename",function (req,res) {
-    fs.unlink(`./tasks/${req.params.filename}`,function (err,filedata) {
-        console.log('file deleted');
-        res.redirect("/")
-    }) 
-})
+app.get("/delete/:filename", function (req, res) {
+    const userFolder = `./tasks/${req.cookies.userId}`;
+    fs.unlink(`${userFolder}/${req.params.filename}`, function (err) {
+        res.redirect("/");
+    });
+});
 
 app.get("/edit/:filename", function (req, res) {
-    fs.readFile(`./tasks/${req.params.filename}`, "utf-8", function (err, filedata) {
-        res.render("edit", { filename: req.params.filename, filedata: filedata });
+    const userFolder = `./tasks/${req.cookies.userId}`;
+    fs.readFile(`${userFolder}/${req.params.filename}`, "utf-8", function (err, filedata) {
+        res.render("edit", { filename: req.params.filename, filedata });
     });
 });
 
-
 app.post("/update/:filename", function (req, res) {
+    const userFolder = `./tasks/${req.cookies.userId}`;
     const newTitle = req.body.title;
     const newDesc = req.body.desc;
 
-
-    fs.unlink(`./tasks/${req.params.filename}`, function (err) {
-        fs.writeFile(`./tasks/${newTitle}`, newDesc, function (err) {
+    fs.unlink(`${userFolder}/${req.params.filename}`, function (err) {
+        fs.writeFile(`${userFolder}/${newTitle}`, newDesc, function (err) {
             res.redirect("/");
         });
     });
 });
 
+app.post("/create", function (req, res) {
+    const userFolder = `./tasks/${req.cookies.userId}`;
+    fs.writeFile(`${userFolder}/${req.body.title}`, req.body.desc, function (err) {
+        res.redirect("/");
+    });
+});
 
-app.post("/create",function (req,res) {
-    fs.writeFile(`./tasks/${req.body.title}`,req.body.desc,function (err) {
-        res.redirect("/")
-    })
-    
-})
-
-app.listen(3000)
+app.listen(3000);
